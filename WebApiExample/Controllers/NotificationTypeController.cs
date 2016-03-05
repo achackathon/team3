9ef139b028bc7191fcd945474a6e871f862cc781
Daniela Marinho <dani.marinho24@gmail.com>
2016-03-05 10:34:57 -0300

    Inserção no team3

---
+++ WebApiExample/Controllers/NotificationTypeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace AvisaAi.WebApi.Controllers
+{
+    [RoutePrefix("api/v1")]
+    public class NotificationTypeController : ApiController
+    {
+        #region [Get]
+
+        [HttpGet]
+        [Route("notificationType")]
+        public HttpResponseMessage Get()
+        {
+            return null;
+        }
+
+        #endregion
+    }
+}
